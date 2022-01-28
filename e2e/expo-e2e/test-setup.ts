6d882503cad921a88ca355cc9bf8b5de4f1f836d
Jack Hsu <jack.hsu@gmail.com>
2022-01-28 09:08:42 -0500

    moved repo

--- e2e/expo-e2e/test-setup.ts
+++
@@ -1,9 +0,0 @@
-import {
-  ensureNxProject,
-  runPackageManagerInstall,
-} from '@nrwl/nx-plugin/testing';
-
-beforeEach(() => {
-  ensureNxProject('@nrwl/expo', 'build/packages/expo');
-  runPackageManagerInstall();
-});
